da83ec1f6b546e4e6b11debf499c177849bc604e
张配勇 <673947453@qq.com>
2020-12-15 17:40:53 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
-
+import Qs from 'qs';
 const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
     // baseURL: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
-    timeout: 5000
+    timeout: 5000,
+    'Access-Control-Allow-Credentials':true,
+    withCredentials:true,
+    method:'post',
+    headers:{
+        'Content-Type':'application/x-www-form-urlencoded'
+    },
+    tranformRequest:[function(data){
+        return Qs.stringify(data)
+    }]
 });
 
 service.interceptors.request.use(
